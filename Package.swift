132930c960a92a260af92db116a2e79c948e40de
twof <fabiobean2@gmail.com>
2017-09-13 17:42:13 -0700

    Adding the rest. Frameworks can now be imported properly with spm

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "osxdotool",
-    
     dependencies: [
         .Package(url: "https://github.com/twof/CLSwift.git", majorVersion: 1)
     ]
