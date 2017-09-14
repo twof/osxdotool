3d9302bd5589f688c89880f418ac835dacd6ef4f
twof <fabiobean2@gmail.com>
2017-09-13 18:01:32 -0700

    Folder Structure makes sense

--- Sources/osxdotool/Command.swift
+++ Sources/osxdotool/Command.swift
@@ -23,94 +23,53 @@ class Command {
 //        if params.count > 0 {
 //            let commmandString = params[0]
 //            switch commandString {
-//            case "mousemove":
-//            case "mousemove_relative":
-//            case "getmouselocation":
-//            case "click":
-//            case "key":
-//            case "type":
+//            - [ ] "mousemove":
+//            - [ ] "mousemove_relative":
+//            - [ ] "getmouselocation":
+//            - [ ] "click":
+//            - [ ] "key":
+//            - [ ] "type":
 //            default:
 //            }
 //        }
 //        
 //    }
 //    
-//    case mousemove([String])
-//    case mouseMoveRelative([String])
-//    case getMouseLocation([String])
-//    case click([String])
-//    case key([String])
-//    case type([String])
-//    case mouseDown([String])
-//    case mouseUp([String])
-//    case search([String])
-//    case selectWindow([String])
-//    case behave([String])
-//    case getWindowPID([String])
-//    case getWindowName([String])
-//    case getWindowGeometry([String])
-//    case getWindowFocus([String])
-//    case windowSize([String])
-//    case windowMove([String])
-//    case windowFocus([String])
-//    case windowMap([String])
-//    case windowMinimize([String])
-//    case windowRaise([String])
-//    case windowReparent([String])
-//    case windowKill([String])
-//    case windowUnmap([String])
-//    case setWindow
-//    case windowActivate
-//    case getActiveWindow
-//    case setNumDesktops
-//    case getNumDesktops
-//    case getDesktopViewport
-//    case setDesktopViewport
-//    case setDesktop
-//    case getDesktop
-//    case setDesktopForWindow
-//    case getDesktopForWindow
-//    case sleep
-//    //case exec //unimplemented unless someone really wants it. seems dangerous
-//    
-//    
-//    func run() {
-//        do{
-//            switch self {
-//            case let .mousemove(params):
-//                try validate(params: params, minNumParams: 2, expectedType: Int.self)
-//            default:
-//                <#code#>
-//            }
-//        }catch{
-//            
-//        }
-//        
-//    }
-//    
-//    func validate<T>(params: [String], minNumParams: Int, expectedType: T.Type) throws -> [T]{
-//        if params.count < minNumParams {
-//            throw InputError.tooFewArgs
-//        }
-//        
-//        var castedParams = [T]()
-//        
-//        for param in params {
-//            guard let x: T = T(param) else {throw InputError.invalidType(param, T.self)}
-//        }
-//    }
-//}
-//
-//
-///*
-// Chain of Command (heh):
-// Validate input:
-//    Valid command:
-//        Extract flags:
-//        Correct count:
-//        Correct type:
-//            Success:
-//                Execute
-//            Failure:
-//                Throw error. Print usage
-// */
+    - [ ] mousemove
+    - [ ] mouseMoveRelative
+    - [ ] getMouseLocation
+    - [ ] click
+    - [ ] key
+    - [ ] type
+    - [ ] mouseDown
+    - [ ] mouseUp
+    - [ ] search
+    - [ ] selectWindow
+    - [ ] behave
+    - [ ] getWindowPID
+    - [ ] getWindowName
+    - [ ] getWindowGeometry
+    - [ ] getWindowFocus
+    - [ ] windowSize
+    - [ ] windowMove
+    - [ ] windowFocus
+    - [ ] windowMap
+    - [ ] windowMinimize
+    - [ ] windowRaise
+    - [ ] windowReparent
+    - [ ] windowKill
+    - [ ] windowUnmap
+    - [ ] setWindow
+    - [ ] windowActivate
+    - [ ] getActiveWindow
+    - [ ] setNumDesktops
+    - [ ] getNumDesktops
+    - [ ] getDesktopViewport
+    - [ ] setDesktopViewport
+    - [ ] setDesktop
+    - [ ] getDesktop
+    - [ ] setDesktopForWindow
+    - [ ] getDesktopForWindow
+    - [ ] sleep
+    //- [ ] exec //unimplemented unless someone really wants it. seems dangerous
+
